9253e9ad738023b016e7acc2b1e2c0ecec28f6dd
DRMPN <mr.gorden.smit@gmail.com>
2025-08-15 03:29:30 +0300

    feat: add osa and workflow settings to configuration tab

--- configuration_tab.py
+++ configuration_tab.py
@@ -1,64 +1,4 @@
-import tempfile
-
 import streamlit as st
-
-
-@st.dialog("Add an Article")
-def add_article(type) -> None:
-    article = None
-    if type == "URL":
-        article = st.text_input("Paste URL")
-    elif type == "File":
-        article = st.file_uploader("Upload Article", ["pdf"])
-
-    st.container(height=5, border=False)
-
-    if st.button(
-        "Submit",
-        disabled=not article,
-        use_container_width=True,
-        type="primary",
-    ):
-        if type == "File":
-            tmpfilename = tempfile.NamedTemporaryFile(
-                delete=False, dir=st.session_state.tmpdirname, suffix=".pdf"
-            )
-            tmpfilename.write(article.getvalue())
-            article = tmpfilename.name
-        st.session_state.article = {"data": article, "type": type}
-        st.rerun()
-
-
-@st.fragment
-def render_article_block() -> None:
-    with st.container(border=True):
-        st.markdown(
-            '<h5 style="text-align: center;">Article</h5>',
-            unsafe_allow_html=True,
-        )
-        if "article" not in st.session_state:
-            left, right = st.columns(2)
-            with left:
-                if st.button(
-                    "Paste URL",
-                    icon=":material/globe:",
-                    use_container_width=True,
-                ):
-                    add_article("URL")
-            with right:
-                if st.button(
-                    "Upload Article",
-                    icon=":material/upload_file:",
-                    use_container_width=True,
-                ):
-                    add_article("File")
-        else:
-            st.success(
-                f"Article added via {st.session_state.article.get("type")}:  \n{st.session_state.article.get("data")}"
-            )
-            if st.button("Remove", use_container_width=True):
-                del st.session_state["article"]
-                st.rerun()
 
 
 @st.fragment
@@ -68,10 +8,10 @@ def render_git_settings_block() -> None:
             '<h5 style="text-align: center;">Git settings</h5>',
             unsafe_allow_html=True,
         )
-        token_status = (
-            "✅ Found" if st.session_state.git_token else "❌ Not found in .env"
-        )
-        st.info(f"GIT_TOKEN status: {token_status}")
+        if st.session_state.git_token:
+            st.info("**GIT_TOKEN Status**: Found", icon=":material/check_circle:")
+        else:
+            st.info("**GIT_TOKEN Status**: Found", icon=":material/error:")
 
         if not st.session_state.git_token:
             st.warning(
@@ -79,27 +19,23 @@ def render_git_settings_block() -> None:
             )
         st.text_input(
             label="Branch",
-            key="branch",
-            help="""
-                Branch name of the GitHub repository  
-                Default: Default branch""",
+            help="""Branch name of the GitHub repository  
+                `Default: Default branch`""",
         )
         left, right = st.columns(2)
         with left:
             st.checkbox(
                 label="No pull request",
                 key="no_pull_request",
-                help="""
-                Avoid create pull request for target repository  
-                Default: False""",
+                help="""Avoid create pull request for target repository  
+                `Default: False`""",
             )
         with right:
             st.checkbox(
                 label="No fork",
                 key="no_fork",
-                help="""
-                        Avoid create fork for target repository  
-                        Default: False""",
+                help="""Avoid create fork for target repository  
+                        `Default: False`""",
             )
 
 
@@ -110,14 +46,206 @@ def render_osa_settings_block() -> None:
             '<h5 style="text-align: center;">OSA settings</h5>',
             unsafe_allow_html=True,
         )
-        st.checkbox(
-            label="Delete directory",
-            help="""
-                Enable deleting the downloaded repository after processing (Linux only)  
-                Default: False""",
-            value=True,
+        st.text_input(
+            label="Output",
+            value="Current working directory",
             disabled=True,
+            help="""Path to the output directory  
+                    `Default: Current working directory`""",
         )
+        left, right = st.columns(2)
+        with left:
+            st.checkbox(
+                label="Web Mode",
+                help="""Enable web interface mode. When set, the tool will generate  
+                        the task plan without launching the interactive CLI editor  
+                        `Default: False`""",
+                value=True,
+                disabled=True,
+            )
+        with right:
+            st.checkbox(
+                label="Delete directory",
+                help="""
+                Enable deleting the downloaded repository after processing (Linux only)  
+                `Default: False`""",
+                value=True,
+                disabled=True,
+            )
+        left, right = st.columns(2)
+        with left:
+            st.checkbox(
+                label="Generate README",
+                value=True,
+                help="""Generate a `README.md` file based on repository content and metadata  
+                        `Default: False`""",
+            )
+            st.checkbox(
+                label="Organize Repository",
+                value=True,
+                help="""Organize the repository by adding standard `tests` and `examples` directories if missing  
+                        `Default: False`""",
+            )
+            st.checkbox(
+                label="Generate Docstrings",
+                value=True,
+                help="""Automatically generate docstrings for all Python files in the repository  
+                    `Default: False`""",
+            )
+        with right:
+            st.checkbox(
+                label="Refine README",
+                value=False,
+                help="""Enable advanced README refinement. This process requires a powerful LLM model (such as GPT-4 or equivalent) for optimal results  
+                        `Default: False`""",
+            )
+            st.checkbox(
+                label="Translate Directories",
+                help="""Enable automatic translation of directory names into English  
+                    `Default: False`""",
+            )
+            st.checkbox(
+                label="Generate Requirements",
+                value=False,
+                help="""Generate a `requirements.txt` file based on repository content  
+                    `Default: False`""",
+            )
+        st.checkbox(
+            label="Generate PDF Report",
+            value=True,
+            help="""Analyze the repository and generate a PDF report with project insights  
+                    `Default: False`""",
+        )
+        st.checkbox(
+            label="Generate About Section",
+            value=True,
+            help="""Generate GitHub `About` section with tags  
+                    `Default: False`""",
+        )
+        st.checkbox(
+            label="Generate Community Documentation Files",
+            value=True,
+            help="""Generate community-related documentation files,  
+                    such as `Code of Conduct` and `Contributing guidelines`  
+                    `Default: False`""",
+        )
+        st.multiselect(
+            "Convert Notebooks",
+            [],
+            accept_new_options=True,
+            help="""Convert Jupyter notebooks to `.py` format  
+                    Provide paths, or leave empty for repo directory  
+                    **Example: path/to/file1, path/to/file2**  
+                    `Default: —`""",
+        )
+        st.selectbox(
+            label="Ensure License",
+            options=(None, "bsd-3", "mit", "ap2"),
+            help="""
+                Enable LICENSE file compilation  
+                `Default: None`
+                """,
+        )
+
+
+@st.fragment
+def render_workflow_settings_block() -> None:
+    with st.container(border=True):
+        st.markdown(
+            '<h5 style="text-align: center;">Workflow settings</h5>',
+            unsafe_allow_html=True,
+        )
+        workflows = st.checkbox(
+            label="Generate Workflows",
+            help="""
+                Generate GitHub Action workflows for the repository  
+                `Default: False`""",
+            value=False,
+        )
+        if workflows:
+            st.multiselect(
+                label="Python Verisons",
+                default=["3.9", "3.10"],
+                options=["3.9", "3.10", "3.11", "3.12"],
+                help="""Python versions to test against
+                        `Default: [3.9, 3.10]`""",
+            )
+            st.multiselect(
+                label="Branches",
+                options=[],
+                accept_new_options=True,
+                help="""Branches to trigger workflows on
+                        `Default: []`""",
+            )
+            st.text_input(
+                label="Workflow Output Directory",
+                value=".github/workflows",
+                help="""Directory where workflow files will be saved  
+                    `Default: .github/workflows`""",
+            )
+            left, right = st.columns([0.4, 0.6])
+            st.checkbox(
+                label="Include Unit Tests",
+                help="""
+                Include unit tests workflow  
+                `Default: True`""",
+                value=True,
+            )
+            st.checkbox(
+                label="Include PyPi",
+                help="""Include PyPI publish workflow  
+                `Default: False`""",
+                value=False,
+            )
+            with left:
+                st.checkbox(
+                    label="Include codecov",
+                    help="""
+                    Include Codecov coverage step in unit tests workflow  
+                    `Default: True`""",
+                    value=True,
+                )
+                st.checkbox(
+                    label="Include Black",
+                    help="""
+                Include Black formatter workflow  
+                `Default: True`""",
+                    value=True,
+                )
+                st.checkbox(
+                    label="Include PEP 8",
+                    help="""Include PEP 8 compliance workflow  
+                `Default: True`""",
+                    value=True,
+                )
+            with right:
+                st.checkbox(
+                    label="Use codecov Token",
+                    help="""
+                    Include Use Codecov token for coverage upload  
+                    `Default: False`""",
+                    value=False,
+                )
+                st.checkbox(
+                    label="Include autopep8",
+                    help="""Include autopep8 formatter workflow  
+                `Default: False`""",
+                    value=False,
+                )
+                st.checkbox(
+                    label="Include `/fix-pep8` command",
+                    help="""Include fix-pep8 command workflow  
+                `Default: False`""",
+                    value=False,
+                )
+            st.selectbox(
+                label="PEP8 Tool",
+                options=("flake8", "pylint"),
+                help="""
+                Tool to use for PEP 8 checking  
+                `Default: flake8`
+                """,
+            )
 
 
 @st.fragment
@@ -132,7 +260,7 @@ def render_llm_settings_block() -> None:
             options=("itmo", "llama", "openai", "ollama"),
             help="""
                 LLM API service provider  
-                Default: llama
+                `Default: llama`
                 """,
         )
         st.text_input(
@@ -140,42 +268,50 @@ def render_llm_settings_block() -> None:
             value="https://api.openai.com/v1",
             help="""
                 URL of the provider compatible with OpenAI API  
-                Default: https://api.openai.com/v1""",
+                `Default: https://api.openai.com/v1`""",
         )
         st.text_input(
             label="Model",
             value="gpt-3.5-turbo",
             help="""
                 Specific LLM model to use  
-                Default: gpt-3.5-turbo""",
+                `Default: gpt-3.5-turbo`  
+                See:
+                1. https://vsegpt.ru/Docs/Models  
+                2. https://platform.openai.com/docs/models  
+                3. https://ollama.com/library  """,
         )
         st.number_input(
             label="Maximum number of tokens",
             value=4096,
             help="""
                 Maximum number of tokens the model can generate in a single response  
-                Default: 4096""",
+                **Example: 1024**  
+                `Default: 4096`""",
         )
         st.selectbox(
             label="Temperature",
             options=(None, 0, 1),
             help="""
                 Sampling temperature to use for the LLM output (0 = deterministic, 1 = creative)  
-                Default: None""",
+                `Default: None`""",
         )
-        st.text_input(
+        st.number_input(
             label="Top-p (Nucleus Sampling)",
+            value=None,
             help="""
-                Nucleus sampling probability  
-                Default: None""",
+                Nucleus sampling probability (1.0 = all tokens considered)  
+                *Example: 0.8**  
+                `Default: None`""",
         )
 
 
 def render_configuration_tab() -> None:
     left, center, right = st.columns([1, 1, 1])
     with left:
+        render_git_settings_block()
         render_osa_settings_block()
     with center:
-        render_git_settings_block()
+        render_workflow_settings_block()
     with right:
         render_llm_settings_block()
